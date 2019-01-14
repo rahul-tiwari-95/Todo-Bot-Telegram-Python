61b4137695ff3283f4709a963746bde591a64f9b
Rahul Tiwari <rahultiwari@Rahuls-MBP.lan1>
2019-01-14 01:14:03 -0500

    Run todobot.py

---
+++ echobot.py
@@ -0,0 +1,57 @@
+import json 
+import requests
+import time
+import urllib
+
+TOKEN = "630092935:AAHN_uram1KGBLGSsuWUMQ7k28dp1cC1wu0"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    # print(url)
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates():
+    url = URL + "getUpdates"
+    js = get_json_from_url(url)
+    print(js)
+    return js
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+
+    return (text, chat_id)
+
+
+def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+    
+
+def main():
+    last_textchat = (None, None)
+    while True:
+        text, chat = get_last_chat_id_and_text(get_updates())
+        if (text, chat) != last_textchat:
+            send_message(text, chat)
+            last_textchat = (text, chat)
+        time.sleep(1.5)
+
+
+if __name__ == '__main__':
+    # print("BOT RESPONSE" , get_updates())
+    main()
